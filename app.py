aa1de7aa3696e0988ab214c04fe664c8bd694f8d
Hồ Gia Bảo <quaybao24@gmail.com>
2025-07-29 18:14:05 +0700

    Kết nối Flask với database

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
 from flask import Flask, render_template, request, redirect
 
+import pymysql
+import os
+
+conn = pymysql.connect(
+    host=os.getenv('DB_HOST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASS'),
+    database=os.getenv('DB_NAME'),
+    charset='utf8mb4',
+    cursorclass=pymysql.cursors.DictCursor
+)
+
 app = Flask(__name__)
 books = [
     {
